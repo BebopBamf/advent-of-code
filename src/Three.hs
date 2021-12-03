e945d6a115f6ba13e444901a874534f532470be2
Euan Mendoza <bebopbamf@effectfree.dev>
2021-12-04 06:24:52 +1100

    RIP Internet gave up last night

--- src/Three.hs
+++ src/Three.hs
@@ -1,5 +1,19 @@
 module Three (threePartOne) where
 
 
-threePartOne :: String -> [String]
-threePartOne = lines
+threePartOne :: String -> [Int]
+threePartOne = sumArray . parseToIntArray
+
+
+parseToIntArray :: String -> [[Int]]
+parseToIntArray = map parseToInt . lines
+
+
+parseToInt :: String -> [Int]
+parseToInt = map (\c -> read [c])
+
+
+sumArray :: [[Int]] -> [Int]
+sumArray [] = []
+sumArray [x] = x
+sumArray (x : xs) = zipWith (+) x $ sumArray xs
